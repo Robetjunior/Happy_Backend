fae41673841330baa79edffb1ee3571eddb6b8bd
Jose Roberto <juniorferreirarob@gmail.com>
2020-10-14 19:41:59 -0300

    upload de images feito

--- src/controllers/OrphanagesControllers.ts
+++ src/controllers/OrphanagesControllers.ts
@@ -22,6 +22,8 @@ export default {
     },
 
     async create(req: Request, res: Response){
+        console.log(req.files)
+
         const { 
             name, 
             latitude, 
@@ -34,6 +36,12 @@ export default {
     
         const orphanagesRepository = getRepository(Orphanage)
     
+        const requestImages = req.files as Express.Multer.File[]; // e um array de arquivos
+        
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
         const orphanage = orphanagesRepository.create({
             name, 
             latitude, 
@@ -42,6 +50,7 @@ export default {
             instructions, 
             opening_hours, 
             open_on_weekends,
+            images
         })
     
         await orphanagesRepository.save(orphanage);
